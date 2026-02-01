688a4bc1adae5de21792d3992343dfb64d05d600
Sanjeev <sanjeevrises2499@gmail.com>
2026-02-01 08:31:00 +0530

    Settingup basic html strucutre with some flask basics

--- app.py
+++ app.py
@@ -1,42 +1,35 @@
-from flask import Flask , Response , request, session,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 
-app=Flask(__name__)
-app.secret_key="supersecret"
-#Home Page
-@app.route("/",methods=["GET","POST"])
+app = Flask(__name__)
+
+@app.route("/")
 def login():
-    if request.method =="POST":
-        username =request.form.get("username")
-        password =request.form.get("password")
+    return render_template("login.html")
 
-        if username == "admin" and password=="123":
-            session["user"] =username
-            return redirect(url_for("welcome"))
-        else:
-            return Response("In-valid credentials. Try again",mimetype="text/plain")
-    return '''
-            <h2> Login Page</h2>
-            <form method="POST">
-            Username:<input type="text" name="username"><br>
-            Password: <input type="text" name="password"><br>
-            <input type="submit" value="Login">
-            </form>
-'''
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
 
-#Welcome Page
+@app.route("/submit", methods=["POST"])
+def submit():
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/welcome",methods=["GET","POST"])
-def  welcome():
-    if "user" in session:
-        return f'''
-        <h2>Welcome,{session["user"]}!</h2>
-        <a href={url_for('logout')}>Logout</a>
 
-'''
-    return redirect(url_for("login"))
+    # if username == "admin" and password == "123":
+    #     return redirect(url_for("welcome"))
+    # else:
+    #     return redirect(url_for("login"))
 
-#Logout Page
-@app.route("/logout")
-def logout():
-    session.pop("user")
-    return redirect(url_for("login"))
+    valid_users ={
+        'admin':'123',
+        'sanju':'1234',
+        'sam':'12345'}
+    
+    if username in valid_users and password == valid_users[username]:
+        return render_template("welcome.html")
+    else:
+        return "Invalid Usercredentials"
+    
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=5000)
